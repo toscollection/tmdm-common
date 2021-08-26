e2bb814a0bfa77496424291b05b2f105e8a5cc53
Haiting Yin <htyin@talend.com>
2021-08-26 15:51:15 +0800

    TMDM-15099 Replace token encryption mechanism (#285)

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/AESEncryption.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/AESEncryption.java
@@ -37,15 +37,9 @@ public class AESEncryption {
 
 	public static final String KEYS_FILE = "encryption.keys.file"; //$NON-NLS-1$
 
-	private static final String AES_KEY = "aes.key"; //$NON-NLS-1$
+	public static final String ENCRYPTION_KEY = "mdm.encryption.key"; //$NON-NLS-1$
 
 	private Encryption encryption;
-
-	private SecretKeySpec keySpec;
-
-	private byte[] key;
-
-	private String algorithm;
 
 	public static AESEncryption getInstance() {
 		if (instance == null) {
@@ -57,7 +51,6 @@ public class AESEncryption {
 	public AESEncryption() {
 		try {
 			String keyfile = System.getProperty(KEYS_FILE);
-			KeySource keySource = null;
 			if (StringUtils.isEmpty(keyfile)) {
 				keyfile = System.getProperty("mdm.root") + System.getProperty("file.separator") + "conf"
 						+ System.getProperty("file.separator") + "aeskey.dat";
@@ -68,23 +61,20 @@ public class AESEncryption {
 					config.setDelimiterParsingDisabled(true);
 					config.setEncoding(StandardCharsets.UTF_8.name());
 					config.load(file);
-					if (StringUtils.isEmpty(config.getString(AES_KEY))) {
-						keySource = KeySources.random(32);
-						config.setProperty(AES_KEY, EncodingUtils.BASE64_ENCODER.apply(keySource.getKey()));
+					if (StringUtils.isEmpty(config.getString(ENCRYPTION_KEY))) {
+						config.setProperty(ENCRYPTION_KEY,
+								EncodingUtils.BASE64_ENCODER.apply(KeySources.random(32).getKey()));
 						config.save(file);
 					}
 				}
 			}
-			if (keySource == null) {
-				keySource = KeySources.file(AES_KEY);
-			}
-			encryption = new Encryption(keySource, CipherSources.getDefault());
+			encryption = new Encryption(KeySources.file(ENCRYPTION_KEY), CipherSources.getDefault());
 		} catch (Exception e) {
 			LOGGER.error("Error occurs while initial AESEncryption. ", e);
 		}
 	}
 
-	protected String encrypt(String key, String value) throws IOException {
+	public String encrypt(String key, String value) throws IOException {
 		try {
 			return StringUtils.isEmpty(value) ? StringUtils.EMPTY : encryption.encrypt(value);
 		} catch (Exception e) {
@@ -103,55 +93,6 @@ public class AESEncryption {
 			// assuming it was not encrypted or encrypted with a different encryption
 			LOGGER.debug("Unable to decrypt value using {} encryption..", encryption.getClass().getName(), e);
 			return value;
-		}
-	}
-
-	public static AESEncryption getDESCryptInstance(String sharedSecret) throws ShortBufferException {
-		byte[] key = new byte[8];
-		byte[] bytes;
-		try {
-			bytes = sharedSecret.getBytes(StandardCharsets.UTF_8.name());
-		} catch (UnsupportedEncodingException uee) {
-			throw new ShortBufferException("The shared secret cannot be used as a key");
-		}
-		if (bytes.length < 8)
-			throw new ShortBufferException("The shared secret is too short");
-		System.arraycopy(bytes, 0, key, 0, 8);
-		return new AESEncryption(key, "DES");
-	}
-
-	/** Creates a new instance of Crypt */
-	public AESEncryption(byte[] key, String algorithm) {
-		this.key = key;
-		this.algorithm = algorithm;
-		this.keySpec = new SecretKeySpec(this.key, this.algorithm);
-	}
-
-	/**
-	 * Encrypts the given String to a hex representation
-	 */
-	public String encryptHexString(String text) {
-		return toHex(encryptString(text));
-	}
-
-	/** Converts the given array of bytes to a hex String */
-	public static String toHex(byte[] buf) {
-		char[] cbf = new char[buf.length * 2];
-		for (int jj = 0, kk = 0; jj < buf.length; jj++) {
-			cbf[kk++] = "0123456789ABCDEF".charAt((buf[jj] >> 4) & 0x0F);
-			cbf[kk++] = "0123456789ABCDEF".charAt(buf[jj] & 0x0F);
-		}
-		return new String(cbf);
-	}
-
-	/** Encrypts the give String to an array of bytes */
-	private byte[] encryptString(String text) {
-		try {
-			Cipher cipher = Cipher.getInstance(this.algorithm);
-			cipher.init(Cipher.ENCRYPT_MODE, this.keySpec);
-			return cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
-		} catch (Exception e) {
-			return null;
 		}
 	}
 
