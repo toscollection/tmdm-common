267cfeb75dfdd52b2098d6a8fdc5c6063dce9c10
htyin <htyin@talend.com>
2022-04-26 14:18:12 +0800

    Revert "TMDM-15244 Error java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2 when deploying new version of Data Model (#411)"
    This reverts commit 7cda0b825897f1487186ea9f8438a66861cbd95a.

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/MetadataUtils.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/MetadataUtils.java
@@ -432,7 +432,7 @@ public class MetadataUtils {
          * networks", Communications of the ACM"
          */
         List<ComplexTypeMetadata> sortedTypes = new LinkedList<>();
-        Set<ComplexTypeMetadata> noIncomingEdges = new HashSet<>();
+        List<ComplexTypeMetadata> noIncomingEdges = new LinkedList<>();
         int lineNumber = 0;
         for (byte[] line : dependencyGraph) {
             if (!hasIncomingEdges(line)) {
@@ -499,9 +499,6 @@ public class MetadataUtils {
                                     int currentDependency = getId(containingType, types);
                                     List<ComplexTypeMetadata> list = new LinkedList<>(dependencyPath);
                                     list.add(containingType);
-                                    if (currentDependency >= dependencyGraph.length) {
-                                        continue;
-                                    }
                                     if (hasIncomingEdges(dependencyGraph[currentDependency]) && !map.containsKey(list)) {
                                         if (dependencyGraph[currentDependency][currentLineNumber] > 0) {
                                             dependencyGraph[currentDependency][currentLineNumber]--;
