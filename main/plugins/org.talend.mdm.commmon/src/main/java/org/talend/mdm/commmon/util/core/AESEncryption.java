d389deb6157d411c168aeef70b2045d7c2c4b5a9
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2022-08-10 14:38:40 +0800

    TMDM-15320 MDM connecting to TDS with user/password contains special characters failed (#513)

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/AESEncryption.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/AESEncryption.java
@@ -10,6 +10,7 @@
 package org.talend.mdm.commmon.util.core;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,6 +59,7 @@ public class AESEncryption {
                 File file = new File(keyfile);
                 if (file.exists()) {
                     Configurations configs = new Configurations();
+                    FileBasedConfigurationBuilder.setDefaultEncoding(PropertiesConfiguration.class, StandardCharsets.UTF_8.name());
                     FileBasedConfigurationBuilder<PropertiesConfiguration> propertiesBuilder = configs.propertiesBuilder(file);
                     PropertiesConfiguration config = propertiesBuilder.getConfiguration();
                     if (StringUtils.isEmpty(config.getString(ENCRYPTION_KEY))) {
